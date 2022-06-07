dbb1abb773a2ae628793934e931326219594e447
flavy <flaviobcn012@gmail.com>
2022-06-07 10:08:58 +0200

    video 23

---
+++ src/observables/02-unsubscribe-add.ts
@@ -0,0 +1,44 @@
+import { Observable, Observer } from "rxjs";
+
+const observer: Observer<any> = {
+  next: (value) => console.log("next: ", value),
+  error: (error) => console.warn("error: ", error),
+  complete: () => console.info("completado: "),
+};
+
+const intervalo$ = new Observable<number>((subscriber) => {
+  let count = 0;
+
+  const interval = setInterval(() => {
+    count++;
+    subscriber.next(count);
+    console.log(count);
+  }, 1000);
+  //
+  setTimeout(() => {
+    subscriber.complete();
+    //  subscription.unsubscribe();
+    //  subscription2.unsubscribe();
+    //  subscription3.unsubscribe();
+  }, 2500);
+  //
+  return () => {
+    clearInterval(interval);
+    console.log("intérvalo destruido");
+  };
+  //
+});
+
+const subscription = intervalo$.subscribe(observer);
+//
+const subscription2 = intervalo$.subscribe(observer);
+//
+const subscription3 = intervalo$.subscribe(observer);
+//
+// concadenado con add()
+subscription.add(subscription2.add(subscription3));
+
+setTimeout(() => {
+  subscription.unsubscribe();
+  console.log("completado timeout");
+}, 4000);
---
+++ src/observables/03-subject.ts
@@ -0,0 +1,36 @@
+import { Observable, Observer, Subject } from "rxjs";
+
+const observer: Observer<any> = {
+  next: (value) => console.log("next: ", value),
+  error: (error) => console.warn("error: ", error),
+  complete: () => console.info("completado: "),
+};
+
+const intervalo$ = new Observable<number>((subs) => {
+  const intervalID = setInterval(() => subs.next(Math.random()), 1000);
+  return () => {
+    clearInterval(intervalID);
+    console.log("Intervalo destruido");
+  };
+});
+
+//
+/* Subject() tipo especial observable
+1) Casteo múltiple: muchos 'subscriber' se subscriben al mismo 'observable' y van a tener todos la misma información
+2) También es un observer
+3) Se puede manejar 'Next','error' y 'complete'
+*/
+const subject$ = new Subject();
+const subscription = intervalo$.subscribe(subject$);
+
+// const subs1 = intervalo$.subscribe((rnd) => console.log("subscription", rnd));
+// const subs2 = intervalo$.subscribe((rnd) => console.log("suscrizione2", rnd));
+
+const subs1 = subject$.subscribe((rnd) => console.log("subscription", rnd));
+const subs2 = subject$.subscribe(observer);
+
+setTimeout(() => {
+  subject$.next(10);
+  subject$.complete();
+  subscription.unsubscribe();
+}, 3500);
