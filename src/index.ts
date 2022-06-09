f493d493b522446727981196277afe4d536a4593
flavy <flaviobcn012@gmail.com>
2022-06-09 17:58:19 +0200

    video 37

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Observable, range, from, pipe } from "rxjs";
+import { filter } from "rxjs/operators";
+
+// range(1, 10)
+//   .pipe(filter((val) => val % 2 === 1))
+//   .subscribe(console.log); // 1, 3, 5, 7, 9
+
+range(1, 10)
+  .pipe(
+    filter((val, i) => {
+      console.log("index", i);
+      return val % 2 === 1;
+    })
+  )
+  .subscribe(console.log);
+
+interface Personaje {
+  tipo: string;
+  nombre: string;
+}
+
+const personajes: Personaje[] = [
+  {
+    tipo: "heroe",
+    nombre: "Batman",
+  },
+  {
+    tipo: "heroe",
+    nombre: "Robin",
+  },
+  {
+    tipo: "villano",
+    nombre: "Joker",
+  },
+];
+
+from(personajes)
+  .pipe(filter((p) => p.tipo === "heroe"))
+  .subscribe(console.log);
